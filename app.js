cf6ef55713bdb48133f1cdf9a59a5f3ea3b32cbc
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-12-01 11:18:41 +0100

    readme update

---
+++ app.js
@@ -0,0 +1,13 @@
+const dropdown = document.querySelector('.js-select')
+const result = document.querySelector('.js-result')
+
+// get data from API: https://dog.ceo/api/breeds/list
+// parse data into JSON
+// based on json response, populate the dropdown
+// when you are done, make dropdown enabled (remove disabled attribute)
+
+// add dropdown event listener - change
+// get dropdown selected option - it holds name of the breed
+// get data from API, use template string for API URL: `https://dog.ceo/api/breed/${breedName}/images/random`
+// prase data into JSON
+// display the photo
